970c0ba30574f27da348c88dd2c2d95bfb0fe279
Maria Luiza Arnhold <101952113+smariarnhold@users.noreply.github.com>
2024-09-26 10:43:25 -0300

    Add files via upload

---
+++ exercicio1.php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+<?php
+//print do texto com echo
+echo "<h3>Finalizando a atividade 1</h3>";
+//print da imagem por link
+echo "<img src=https://picsum.photos/500/600>";
+
+?>    
+    
+</body>
+</html>
