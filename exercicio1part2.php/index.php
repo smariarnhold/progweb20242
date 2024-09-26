59130bf7a1bb6c772a6a889cbf58f1707ec7d147
Maria Luiza Arnhold <101952113+smariarnhold@users.noreply.github.com>
2024-09-26 12:53:33 -0300

    Add files via upload

---
+++ exercicio1part2.php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 2</title>
+</head>
+<body>
+<?php
+//print do texto com echo
+echo "<h1>URLs Absolutas</h1>";
+
+//print dos links absolutos
+echo "<a href= https://www.w3schools.com/>W3C</a><br><br>";
+echo "<a href= https://www.google.com/>Google</a>";
+
+//print do texto com echo
+echo "<h1>URLs Relativas</h1>";
+
+//print dos links relativos
+echo "<a href= https://www.w3schools.com/html/html_images.asp>HTML Images</a><br><br>";
+echo "<a href= https://www.w3schools.com/css/>CSS Tutorial</a>";
+
+?>   
+</body>
+</html>
